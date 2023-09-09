954600a2a6e44ae2aa1972875d8634eea0fa0748
driller <eleshis@gmail.com>
2023-09-09 13:49:58 +0900

    test: bsm

---
+++ tests/test_bsm.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from unittest import TestCase
+from unittest.mock import patch
+
+import numpy as np
+import pytest
+
+import jquants_derivatives
+from jquants_derivatives import Option, bsm, client, models
+
+
+@pytest.fixture()
+def cli():
+    jquants_derivatives.database.db = Path(__file__).resolve().parent / "jquantsapi.db"
+
+    class Client(TestCase):
+        @patch("jquants_derivatives.Client")
+        @client.cast_dataframe(models.IndexOption)
+        def get_option_index_option(self, *args, **kwargs):
+            return jquants_derivatives.database.load(
+                "OPTION_INDEX_OPTION", "2023-01-04"
+            )
+
+    return Client()
+
+
+def test_bsm(cli):
+    df = cli.get_option_index_option()
+    option = Option(df, contracts=2)
+    implied_volatility_put = np.vectorize(bsm.implied_volatility_put)
+    implied_volatility_call = np.vectorize(bsm.implied_volatility_call)
+
+    contract = "2023-01"
+    data = option.contracts_dfs[contract]
+    over_5 = data.loc[data.loc[:, "TheoreticalPrice"] >= 5, :]
+    groupby_div = over_5.groupby("PutCallDivision")
+    put = groupby_div.get_group(1)
+    call_ = groupby_div.get_group(2)
+    s = option.underlying_price[contract]
+    r = option.interest_rate[contract]
+    t = option.time_to_maturity[contract]
+    k_put = put.loc[:, "StrikePrice"]
+    k_call = call_.loc[:, "StrikePrice"]
+    price_put = put.loc[:, "TheoreticalPrice"]
+    price_call = call_.loc[:, "TheoreticalPrice"]
+    sigma_put = put.loc[:, "ImpliedVolatility"]
+    sigma_call = call_.loc[:, "ImpliedVolatility"]
+    np.testing.assert_array_almost_equal(
+        implied_volatility_put(s, k_put, t, r, price_put), sigma_put.values, decimal=2
+    )
+    np.testing.assert_array_almost_equal(
+        implied_volatility_call(s, k_call, t, r, price_call), sigma_call.values, decimal=2
+    )
+    np.testing.assert_array_almost_equal(
+        bsm.price_put(s, k_put, t, r, sigma_put), price_put.values, decimal=1
+    )
+    np.testing.assert_array_almost_equal(
+        bsm.price_call(s, k_call, t, r, sigma_call), price_call.values, decimal=1
+    )
